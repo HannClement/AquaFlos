346643a4e6ca6e224ce4cc7cfd1cf06b1c56903c
HannClement <clementgunadi@gmail.com>
2024-04-19 18:10:44 +0700

    List View Horizontal

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -15,9 +15,9 @@ class MainApp extends StatelessWidget {
         appBar: AppBar(
           centerTitle: true,
           title: Padding(
-            padding: const EdgeInsets.only(top: 10.0), // Atur padding di sini
+            padding: const EdgeInsets.only(top: 10.0),
             child: Text(
-              'Menu',
+              'Flos Aquae',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 30.0,
@@ -25,34 +25,88 @@ class MainApp extends StatelessWidget {
             ),
           ),
         ),
-        body: Container(
-          
+        body: Column(
+          children: [
+            
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  hintText: "Search For Restaurant",
+                  prefixIcon: Icon(Icons.search),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 10.0,),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Most Popular",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // Tambahkan logika navigasi ke halaman "See All" di sini
+                    },
+                    child: Text(
+                      "See All",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                        color: Colors.greenAccent,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 5.0),
+
+            Row(
+              children: [
+                
+              ],
+            ),
+          ],
         ),
         bottomNavigationBar: BottomNavigationBar(
           items: [
             BottomNavigationBarItem(
               label: "Home",
-              icon: Icon(Icons.home)
+              icon: Icon(Icons.home),
             ),
             BottomNavigationBarItem(
               label: "Discovery",
-              icon: Icon(Icons.location_city)
+              icon: Icon(Icons.location_city),
             ),
             BottomNavigationBarItem(
               label: "Bookmark",
-              icon: Icon(Icons.bookmark)
+              icon: Icon(Icons.bookmark),
             ),
             BottomNavigationBarItem(
               label: "Top Foodie",
-              icon: Icon(Icons.favorite)
+              icon: Icon(Icons.emoji_events),
             ),
             BottomNavigationBarItem(
               label: "Profile",
-              icon: Icon(Icons.person)
-            )
+              icon: Icon(Icons.person),
+            ),
           ],
-        selectedLabelStyle: TextStyle(color: Colors.amber),
-        unselectedLabelStyle: TextStyle(color: Colors.blueGrey),
+          selectedLabelStyle: TextStyle(color: Colors.amber),
+          unselectedLabelStyle: TextStyle(color: Colors.blueGrey),
         ),
       ),
     );
