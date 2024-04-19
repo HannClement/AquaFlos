b17be23e86270bc8b85ec4c872bbeae00efce1ba
HannClement <clementgunadi@gmail.com>
2024-04-19 16:04:25 +0700

    Introduction

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flos Aquae",
+      home: Scaffold(
+        body: Center(child: Text("Menu")),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              label: "Home",
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: "Settings",
+              icon: Icon(Icons.settings),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
