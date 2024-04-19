69ae8b0dea35542519548b1cf7307f67376d9120
HannClement <clementgunadi@gmail.com>
2024-04-19 16:39:50 +0700

    Bottom Nav

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -12,18 +12,47 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: "Flos Aquae",
       home: Scaffold(
-        body: Center(child: Text("Menu")),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Padding(
+            padding: const EdgeInsets.only(top: 10.0), // Atur padding di sini
+            child: Text(
+              'Menu',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+              ),
+            ),
+          ),
+        ),
+        body: Container(
+          
+        ),
         bottomNavigationBar: BottomNavigationBar(
           items: [
             BottomNavigationBarItem(
               label: "Home",
-              icon: Icon(Icons.home),
+              icon: Icon(Icons.home)
             ),
             BottomNavigationBarItem(
-              label: "Settings",
-              icon: Icon(Icons.settings),
+              label: "Discovery",
+              icon: Icon(Icons.location_city)
             ),
+            BottomNavigationBarItem(
+              label: "Bookmark",
+              icon: Icon(Icons.bookmark)
+            ),
+            BottomNavigationBarItem(
+              label: "Top Foodie",
+              icon: Icon(Icons.favorite)
+            ),
+            BottomNavigationBarItem(
+              label: "Profile",
+              icon: Icon(Icons.person)
+            )
           ],
+        selectedLabelStyle: TextStyle(color: Colors.amber),
+        unselectedLabelStyle: TextStyle(color: Colors.blueGrey),
         ),
       ),
     );
